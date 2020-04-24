cd49705a133b4002fede79395618267d0a1c0701
floydya <xfloydya@gmail.com>
2020-04-24 18:39:12 +0300

    .

--- store/actions/thunk.d.ts
+++ store/actions/thunk.d.ts
@@ -1,7 +1,6 @@
-import { Dispatch } from "../types";
 declare const _default: {
     login: (loginURL: string, loginData: any) => import("redux-thunk").ThunkAction<any, import("../types").IRootState, undefined, import("../types").ActionType>;
-    logout: () => (dispatch: Dispatch) => void;
+    logout: () => import("redux-thunk").ThunkAction<void, import("../types").IRootState, undefined, import("../types").ActionType>;
     verify: (verifyURL: string, accessToken?: string | null | undefined) => import("redux-thunk").ThunkAction<Promise<any>, import("../types").IRootState, undefined, import("../types").ActionType>;
     refresh: (refreshURL: string, refreshToken?: string | null | undefined) => import("redux-thunk").ThunkAction<Promise<any>, import("../types").IRootState, undefined, import("../types").ActionType>;
     fetchUser: (fetchUserURL: string, accessToken?: string | null | undefined) => import("redux-thunk").ThunkAction<Promise<any>, import("../types").IRootState, undefined, import("../types").ActionType>;
